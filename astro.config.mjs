81bd4592cf6312d0d06355e0b116eceb2acee4c6
罗粟海（Rocky） <Rocky.luo@aishu.cn>
2023-04-03 15:57:48 +0800

    [changed]更新网站地址

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://www.rockyrori.cn/',
+    site: 'https://RockyRori.github.io',
+    base: '/rockyrori',
 });
